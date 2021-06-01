fb416d3d23b53db48279666a81cbbec9fa1d6a2b
diegoalmeida <6208687+diegoalmeida@users.noreply.github.com>
2021-06-01 12:36:34 +0100

    Update portal-header-2lvl-nojquery.js

--- js/src/portal-header-2lvl-nojquery.js
+++ js/src/portal-header-2lvl-nojquery.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// SEARCH   
-$(document).on('click', '.main-menu__container__search_open', function clickSearchOpen() {
+// SEARCH
+document.querySelector('.main-menu__container__search_open').addEventListener('click', function() {
   var window_size = window.matchMedia('(min-width: 992px)');
   if(window_size.matches == true){
     clearOpenSubmenu('main-menu__overlay');
@@ -9,11 +9,11 @@ $(document).on('click', '.main-menu__container__search_open', function clickSear
   } else {
     mobileSearch();
   }
-});
+}, false);
 
-$(document).on('click', '.main-menu__container__search_cancel', function clickSearchCancel() {
+document.querySelector('.main-menu__container__search_cancel').addEventListener('click', function() {
   closeSearch();
-});
+}, false);
 
 function closeSearch() {
   var window_size = window.matchMedia('(min-width: 992px)');
