3b39f86032d2d39755c91a278ff97c22cbc05147
diegoalmeida <6208687+diegoalmeida@users.noreply.github.com>
2021-06-22 19:49:18 +0100

    Update portal-header-login-nojquery.js

--- js/src/portal-header-login-nojquery.js
+++ js/src/portal-header-login-nojquery.js
@@ -1,48 +1,73 @@
 setTimeout(function(){
     /* Login Phone */
-    document.querySelector('header .header-login-dropdown').addEventListener('click', function() {
-        if (this.classList.contains('DropdownActive')) {
-            $('header .header-login-dropdown .header-login-dropdown-menu').fadeOut('fast');
-            $('header .header-login-dropdown .header-login-dropdown-menu .Icon').fadeOut('fast');
-            document.querySelector('header .header-login-dropdown').classList.remove('DropdownActive' );
-        } else {
-            $('header .header-login-dropdown .header-login-dropdown-menu').fadeIn('fast');
-            this.classList.add('DropdownActive');
-            $('header .header-login-dropdown .header-login-dropdown-menu .Icon').fadeIn('fast');
-        }
-    });
+    if(document.querySelector('.header-login > div').classList.contains('header-login-dropdown')) {
+        var logDropMenu = document.querySelector('header .header-login-dropdown .header-login-dropdown-menu');
+        logDropMenu.style.transition = 'all 200ms ease';
+        logDropMenu.style.pointerEvents = 'none';
+        logDropMenu.style.opacity = '0';
+        document.querySelector('header .header-login-dropdown').addEventListener('click', function() {
+            if (this.classList.contains('DropdownActive')) {
+                this.classList.remove('DropdownActive');
+                logDropMenu.style.pointerEvents = 'none';
+                logDropMenu.style.opacity = '0';
+            } else {
+                this.classList.add('DropdownActive');
+            }
+        });
+    }
     
     /* Login Desktop / Tablet */
-    document.querySelector('header .header-login-dropdown-desktop').addEventListener('click', function() {
-        $('header .header-login-dropdown-desktop .header-login-dropdown-menu').fadeIn('fast');
-        this.classList.add('DropdownActive');
-        $('header .header-login-dropdown-desktop .header-login-dropdown-menu .Icon').fadeIn('fast');
-    });
-    
-    $('header .header-login-dropdown-desktop').mouseleave(function() {
-        timeoutHandle = setTimeout(function() { $('header .header-login-dropdown-desktop .header-login-dropdown-menu').fadeOut('fast');
-                                                $('header .header-login-dropdown-desktop .header-login-dropdown-menu .Icon').fadeOut('fast'); 
-                                                document.querySelector('header .header-login-dropdown-desktop').removeClass('DropdownActive' ); }, 500);
-            
-    });
-    $('header .header-login-dropdown-desktop').mouseover(function() {
-        clearTimeout(timeoutHandle);
-    });
-
-    $('header .header-login-dropdown-desktop').hover(function(){ 
-        mouse_is_inside=true; 
-    }, function(){ 
-        mouse_is_inside=false; 
-    });
-
-    $('body').mouseup(function(){
-        if(! mouse_is_inside) {
-            $('header .header-login-dropdown-desktop .header-login-dropdown-menu').fadeOut('fast');
-            $('header .header-login-dropdown-desktop .header-login-dropdown-menu .Icon').fadeOut('fast');
-            document.querySelector('header .header-login-dropdown-desktop').classList.remove('DropdownActive');
+    if(document.querySelector('.header-login > div').classList.contains('header-login-dropdown-desktop')) {
+        var logDropDeskMenu = document.querySelectorAll('header .header-login-dropdown-desktop .header-login-dropdown-menu');
+        for(var i = 0; i < logDropDeskMenu.length; i++) {
+            logDropDeskMenu[i].style.transition = 'all 200ms ease';
+            logDropDeskMenu[i].style.pointerEvents = 'none';
+            logDropDeskMenu[i].style.opacity = '0';
         }
-    });
-    
+        var logDropDeskMenuAll = document.querySelectorAll('header .header-login-dropdown-desktop');
+        for(var i = 0; i < logDropDeskMenuAll.length; i++) {
+            logDropDeskMenuAll[i].addEventListener('click', function() {
+                this.classList.add('DropdownActive');
+                this.querySelector('.header-login-dropdown-menu').style.pointerEvents = '';
+                this.querySelector('.header-login-dropdown-menu').style.opacity = '1';
+            });
+        }
+        
+        var logDropDeskMenuAll = document.querySelectorAll('header .header-login-dropdown-desktop');
+        for(var i = 0; i < logDropDeskMenuAll.length; i++) {
+            logDropDeskMenuAll[i].addEventListener('mouseleave', function() {
+                var MenuElem = this;
+                timeoutHandle = setTimeout(function() {
+                    MenuElem.classList.remove('DropdownActive');
+                    MenuElem.querySelector('.header-login-dropdown-menu').style.pointerEvents = 'none';
+                    MenuElem.querySelector('.header-login-dropdown-menu').style.opacity = '0';
+                }, 500);
+            });
+        }
+        var logDropDeskMenuAll = document.querySelectorAll('header .header-login-dropdown-desktop');
+        for(var i = 0; i < logDropDeskMenuAll.length; i++) {
+            logDropDeskMenuAll[i].addEventListener('mouseover', function() {
+                clearTimeout(timeoutHandle);
+            });
+        }
+
+        var logDropDeskMenuAll = document.querySelectorAll('header .header-login-dropdown-desktop');
+        for(var i = 0; i < logDropDeskMenuAll.length; i++) {
+            logDropDeskMenuAll[i].addEventListener('mouseover', function() {
+                mouse_is_inside=true; 
+            }, function(){ 
+                mouse_is_inside=false; 
+            });
+        }
+        document.querySelector('body').addEventListener('mouseup', function() {
+            if(! mouse_is_inside) {
+                var logDropDeskMenuAll = document.querySelectorAll('header .header-login-dropdown-desktop');
+                for(var i = 0; i < logDropDeskMenuAll.length; i++) {
+                    logDropDeskMenuAll[i].classList.remove('DropdownActive');
+                }
+            }
+        });
+    }
     var timeoutHandle =  "";
     var mouse_is_inside = false;
 }, 1001);
